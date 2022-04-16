c543d1f08f6790ecd9fce9042529adfb8b666909
tecknovice <tecknovice@gmail.com>
2022-04-16 18:52:03 +0700

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,9 +17,10 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { id: user.id, email: user.email };
+    const payload = { id: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
   }
+
 }
